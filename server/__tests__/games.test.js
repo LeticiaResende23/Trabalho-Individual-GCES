ee6c10d7597fe95843110209ae86478a9d7027e2
Letícia <89492943+LeticiaResende23@users.noreply.github.com>
2026-06-07 14:48:41 -0300

    Etapa 4

---
+++ server/__tests__/games.test.js
@@ -0,0 +1,12 @@
+const { GameCollection } = require('../games');
+
+test('create, retrieve and remove game in GameCollection', () => {
+  const gc = new GameCollection();
+  expect(gc.createGame('g1')).toBe(true);
+  // creating the same game id again should fail
+  expect(gc.createGame('g1')).toBe(false);
+  const game = gc.getGame('g1');
+  expect(game).toBeDefined();
+  expect(gc.removeGame('g1')).toBe(true);
+  expect(gc.getGame('g1')).toBeUndefined();
+});
